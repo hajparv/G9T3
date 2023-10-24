a3b758217fc713593657f8d6dc74e96d28f6a75a
peiyilee <peiyi.lee.2022@smu.edu.sg>
2023-10-24 17:07:37 +0800

    edited by py

---
+++ PHP/display.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'common.php';
+$dao = new RecordDAO();
+$records = $dao->getAll();
+
+?>
+<html>
+
+<body>
+    <?php
+    echo "
+        <table border='1'>
+            <tr>
+                <th>AppointmentID</th>
+                <th>Clinic Name</th>
+                <th>NRIC</th>
+                <th>Mobile</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Date Of Birth</th>
+                <th>Appointment Date</th>
+                <th>Vaccination Status</th>
+            </tr>
+    ";
+
+    foreach ($records as $record) {
+        echo "
+            <tr>
+                <td>
+                    {$record->getID()}
+                </td>
+                <td>
+                    {$record->getClinicName()}
+                </td>
+                <td>
+                    {$record->getNric()}
+                </td>
+                <td>
+                    {$record->getMobile()}
+                </td>
+                <td>
+                {$record->getName()}
+                </td>
+                <td>
+                    {$record->getAddress()}
+                </td>
+                <td>
+                    {$record->getBirthdate()}
+                </td>
+                <td>
+                    {$record->getAppointmentDate()}
+                </td>
+                <td>
+                {$record->getVaccinationStatus()}
+                </td>
+                
+            </tr>
+        ";
+    }
+    ?>
+</body>
+
+</html>
---
+++ PHP/getRecord.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'common.php';
+$dao = new RecordDAO();
+$records = $dao->getAll(); // Get an Indexed Array of Post objects
+
+$information = [];
+foreach( $records as $record ) {
+    $information = [];
+    $information["appointmentID"] = $record->getID();
+    $information["clinicName"] = $record->getClinicName();
+    $information["nric"] = $record->getNric();
+    $information["mobile"] = $record->getMobile();
+    $information["name"] = $record->getName();
+    $information["dob"] = $record->getBirthdate();
+    $information["address"] = $record->getAddress();
+    $information["datetime"] = $record->getAppointmentDate();
+    $information["vaccinationStatus"] = $record->getVaccinationStatus();
+    $information[] = $information;
+}
+// make posts into json and return json data
+$postJSON = json_encode($information);
+echo $postJSON;
+?>
