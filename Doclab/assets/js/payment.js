677691a569c80e6741b97a7cdfa28cfd4450a740
boldbossy <112809695+boldbossy@users.noreply.github.com>
2023-10-31 20:07:45 +0800

    pushh

--- Doclab/assets/js/payment.js
+++ Doclab/assets/js/payment.js
@@ -2,10 +2,8 @@
 function calculateTotal() {
     var consultationCost = 35.00; // Cost of consultation
     var medicineCost = 20.00;    // Cost of medicine
-    var insuranceCost = 35.00;   // Cost of claimable insurance
-
     // Calculate the total price
-    var total = consultationCost + medicineCost - insuranceCost;
+    var total = consultationCost + medicineCost;
 
     // Update the total amount in the HTML with a dollar sign and rounded to two decimal places
     document.getElementById('total-amount').textContent = total.toFixed(2);
