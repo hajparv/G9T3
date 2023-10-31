a72192fa1d46d61e871f9a13e9a0e0c5a5f31073
peiyilee <peiyi.lee.2022@smu.edu.sg>
2023-10-31 16:52:32 +0800

    edited by py

--- Doclab/test.js
+++ Doclab/test.js
@@ -20,7 +20,7 @@ var application = new Vue({
         console.log(location);
         result += `
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 pt-5">
-            <div class="blog-card has-before has-after" data-reveal="bottom">
+            <div class="blog-card has-before has-after revealed" data-reveal="bottom">
               <div class="meta-wrapper">
                 <div class="card-meta">
                   <ion-icon name="folder-outline"></ion-icon>
