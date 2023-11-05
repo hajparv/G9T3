953940a5fe057bc7785f134e7c8092ad57c6c5b0
boldbossy <112809695+boldbossy@users.noreply.github.com>
2023-11-05 18:41:04 +0800

    removing uncessary things!

--- Doclab/medication.php
+++ Doclab/medication.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = new PDO("mysql:host=localhost;dbname=healthrecord", "root", "root");
+$connect = new PDO("mysql:host=localhost;dbname=healthrecord", "root", "");
 $received_data = json_decode(file_get_contents("php://input"));
 $data = array();
 if($received_data->action == 'fetchall')
